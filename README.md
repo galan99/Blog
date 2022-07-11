8c02996f2435c3c78982b41f8641dde2c05da789
anlen.wang <619531862@qq.com>
2022-07-11 20:06:58 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Blog
+### 安装Vuepress依赖
+```code
+yarn add -D vuepress #npm install -D vuepress
+```
+
+### 启动本地服务
+```code
+yarn dev #npm run dev
+```
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'My Blog',
+  description: 'This is a blog',
+  base: '/Blog/',
+  theme: 'reco',
+  locales: {
+      '/': {
+        lang: 'zh-CN'
+      }
+  },
+}
